2823121afbc932c8624777bb604c78d9c6f91edb
Ted Eriksson <ted.eriksson@me.com>
2014-03-03 20:16:09 +0000

    changed connection info

--- api/connectinfo.php
+++ api/connectinfo.php
@@ -1,6 +1,6 @@
 <?php
-	define('DATABASE_HOSTNAME', '79.170.44.111:3306');
-	define('DATABASE_USER', 'web111-poiadmin');
-	define('DATABASE_PASSWORD', '7q7Fhg/gD');
-	define('DATABASE_DATABASE', 'web111-poiadmin');
+	define('DATABASE_HOSTNAME', 'aacl8upu6zjwsq.c4go5pdtyiow.eu-west-1.rds.amazonaws.com:3306');
+	define('DATABASE_USER', 'poirds1');
+	define('DATABASE_PASSWORD', 'Yfha46D3');
+	define('DATABASE_DATABASE', 'poi');
 ?>
