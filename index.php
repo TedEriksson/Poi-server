81ac910b25f5e69c15ad76f1da68976bd6497878
Ted Eriksson <ted.eriksson@me.com>
2014-03-04 02:06:05 +0000

    updated website

--- index.php
+++ index.php
@@ -4,22 +4,57 @@
 	<title>Points of interest</title>
 	<link rel="shortcut icon" href="/favicon.ico?v=2" type="image/x-icon">
 	<link rel="icon" href="/favicon.ico?v=2" type="image/x-icon">
-	<link rel="stylesheet" type="text/css" href="main.css">
+	<link href='http://fonts.googleapis.com/css?family=Lato:100,300,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="beautify.css">
 </head>
 <body>
-	<div class="homepage">
-		<div class="logo"></div>
-		<ul class="links">
-			<li>
-				<a href="api/">Api</a>
-			</li>
-			<li>
-				<a href="docs/">Docs</a>
-			</li>
-			<li>
-				<a href="about/">About</a>
-			</li>
-		</ul>
+	<div class="container type2">
+		<div class="inner">
+			<h1>Points Of Interest</h1>
+			<img src="res/img/poi_screenshot.png">
+			<p>
+				Combining Augmented Reality, NFC and Android to create a beacon of information at your fingertips.
+			</p>
+		</div>
 	</div>
+	<div class="container type1">
+		<h1>Open to all</h1>
+		<p>
+			Points of interest allows anyone and everyone to become content creators.
+		</p>
+		<h2>
+			All it takes is three steps
+		</h2>
+		<div class="steps">
+			<a href="#app" class="step 1 type2">Get the<br>App</a href="#">
+			<a href="#plaque" class="step 2 type2">Get your<br>plaque</a href="#">
+			<a href="#point" class="step 3 type2">Create your<br>Point</a href="#">
+		</div>
+	</div>
+	<div id="app" class="container type2">
+		<div class="inner">
+			<h1>Get the App</h1>
+			<p>Download link will be available soon...</p>
+		</div>
+	</div>
+	<div id="plaque" class="container type1">
+		<div class="inner">
+			<h1>Get your plaque</h1>
+			<p>Download link will be available soon...</p>
+		</div>
+	</div>
+	<div id="point" class="container type2">
+		<div class="inner">
+			<h1>Create your point</h1>
+			<p>A help guide will be available soon...</p>
+		</div>
+	</div>
+	<footer class="type2">
+		<div class="inner">
+			<a href="/docs">Documentation</a>
+			<a href="/api">Api</a>
+			<div>Copyright Ted Eriksson 2014</div>
+		</div>
+	</footer>
 </body>
 </html>
