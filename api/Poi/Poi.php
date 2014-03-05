f29a3178879d7db9cd12d63f9e7f472f30dd5c78
Ted Eriksson <ted.eriksson@me.com>
2014-03-05 17:33:53 +0000

    insert safey

--- api/Poi/Poi.php
+++ api/Poi/Poi.php
@@ -49,6 +49,7 @@ class Poi {
 
 	public function insert($vals, $token = null) {
 		$vals = json_decode($vals);
+		if(isset($vals["point_id"])) unset($vals["point_id"]);
 		$insertString = "INSERT INTO points ";
 		$first = true;
 		$keys = "";
