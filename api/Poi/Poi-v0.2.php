66b5af11990d85b58fb1bb541eb7d820b7f15c5b
Ted Eriksson <ted.eriksson@me.com>
2014-03-14 16:44:09 +0000

    changes to poiobject

--- api/Poi/Poi-v0.2.php
+++ api/Poi/Poi-v0.2.php
@@ -4,7 +4,7 @@ include 'DAO.php';
 class Poi {
 	public function getPoint($pointID) {
 		$points = new pointsDAO(false);
-		return json_encode(getByPointID($pointID));
+		return json_encode($points->getByPointID($pointID));
 	}
 }
 
