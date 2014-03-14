daefa93060243a069e3de0d78912f2c57dcb608d
Ted Eriksson <ted.eriksson@me.com>
2014-03-14 17:07:22 +0000

    changes to poiobject

--- api/Poi/Poi-v0.2.php
+++ api/Poi/Poi-v0.2.php
@@ -7,9 +7,28 @@ class Poi {
 		return $this->pointsArrayToJSON($points->getByPointID($pointID));
 	}
 
+	public function getPoints() {
+		return Unauthorized::printError();
+	}
+
 	private function pointsArrayToJSON($arrayOfPoints) {
 		return json_encode(array("points" => $arrayOfPoints));
 	}
+}
+
+abstract class JsonErrorMessage {
+
+	public static function printError() {
+		$error = array();
+		$error['code'] = $this->_code;
+		$error['message'] = $this->_message;
+		return json_encode(array('error' => $error));
+	}
+}
+
+class Unauthorized extends JsonErrorMessage {
+	protected $_code = 400;
+	protected $_message = "Unauthorized. You are not allowed to view this";
 }
 
 ?>
