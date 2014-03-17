675a087724d407c8bebff0267cb5fcd89fe3d9a5
Ted Eriksson <ted.eriksson@me.com>
2014-03-17 20:24:42 +0000

    insert point

--- api/poi_api.php
+++ api/poi_api.php
@@ -50,7 +50,9 @@
 
 	function rest_post($request) {
 		global $poi;
+		echo "post";
 		if($request[0] == POINTS && !isset($request[1])) {
+			echo "insert";
 			echo $poi->insertPoint(file_get_contents("php://input"));
 			return;	
 		}
